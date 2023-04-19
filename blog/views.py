c8372c6ade653f3bf31d1c9fabd1578f7c3e9cdb
Narges1374 <narges.timachi@gmail.com>
2023-04-19 22:21:01 +0430

    add permission_classes

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.http import Http404
 from django.views.generic import TemplateView, ListView, DetailView
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from blog.models import Post
 from blog.serializers import PostSerializer
 
@@ -40,3 +42,4 @@ class PostDetailApiView(RetrieveAPIView):
 class PostViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
